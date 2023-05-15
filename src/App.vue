704a09ea004fe9a28bfac77f8d213a59a1fac3d1
Aditya Kadam <adityakadam@Adityas-Air.lan>
2023-05-15 09:38:18 +0100

    progress bar

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,29 @@
   <div class="max-w-2xl mx-auto py-16">
     <!-- <Calendar class="w-full" /> -->
     <!-- <TagInput /> -->
-    <OTPPad :length="5" @entered="(v)=>otpValue = v"/>
-
-      {{otpValue}}
+    <ProgressBar :progress="progress" />
   </div>
 </template>
 
 <script setup lang="ts">
-import {defineAsyncComponent, ref} from 'vue'
+import {defineAsyncComponent, onMounted, ref} from 'vue'
 
 // const Calendar = defineAsyncComponent(() => import('./components/calendar/index.vue'))
 // const TagInput = defineAsyncComponent(() => import('./components/tag/TagInput.vue'))
-const OTPPad = defineAsyncComponent(() => import('./components/pin/OTPPad.vue'))
+// const OTPPad = defineAsyncComponent(() => import('./components/pin/OTPPad.vue'))
+const ProgressBar = defineAsyncComponent(() => import('./components/progress/ProgressBar.vue'))
 
+const progress = ref(0)
 
-const otpValue = ref('')
+onMounted(() => {
 
+  let interval = setInterval(()=>{
+    progress.value += 5
+    if(progress.value >= 100){
+      clearInterval(interval)
+    }
+  }, 1000)
+})
 
 </script>
 
---
+++ src/components/progress/ProgressBar.vue
@@ -0,0 +1,52 @@
+<template>
+       <div class="bg-gray-200 h-4 rounded-full w-full">
+            <div class="progress" :style="`width: ${progress}%;`">
+            </div>
+       </div>
+</template>
+
+<script setup lang="ts">
+const {progress=0} = defineProps({
+    progress: Number
+})
+</script>
+
+<style scoped>
+@keyframes progress {
+    0%{
+        @apply bg-red-400;
+    }
+    50%{
+        @apply bg-purple-400;
+    }
+    75%{
+        @apply bg-indigo-400;
+    }
+    100%{
+        @apply bg-green-400;
+    }
+}
+
+@-webkit-keyframes progress {
+    0%{
+        @apply bg-red-400;
+    }
+    50%{
+        @apply bg-purple-400;
+    }
+    75%{
+        @apply bg-indigo-400;
+    }
+    100%{
+        @apply bg-green-400;
+    }
+}
+
+
+.progress{
+    @apply h-full rounded-full transition-all ease-in-out duration-150;
+    animation: progress 2s infinite linear;
+}
+
+
+</style>
